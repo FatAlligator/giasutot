7feb42351d4fea88cb918b2ef4e249c6fd23f42f
FatAlligator <amazarashi2001@gmail.com>
2025-05-31 23:14:08 +0700

    add basic login and register feature( fix later)

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,71 @@
-import Image from "next/image";
-import {Header} from "@/app/components/Header";
+// import Image from "next/image";
+// import {Header} from "@/app/components/Header";
+//
+//
+//
+// export default function Home() {
+//   return (
+//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+//       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+//         <Header/>
+//         <div className="flex flex-col gap-[32px]">Không phải gia sư tốt</div>
+//       </main>
+//
+//     </div>
+//   );
+// }
+// import { getTokens } from "next-firebase-auth-edge";
+// import { cookies } from "next/headers";
+// import { notFound } from "next/navigation";
+// import { clientConfig, serverConfig } from "./config";
+//
+// export default async function Home() {
+//     const tokens = await getTokens(await cookies(), {
+//         apiKey: clientConfig.apiKey,
+//         cookieName: serverConfig.cookieName,
+//         cookieSignatureKeys: serverConfig.cookieSignatureKeys,
+//         serviceAccount: serverConfig.serviceAccount,
+//     });
+//
+//     if (!tokens) {
+//         notFound();
+//     }
+//
+//     return (
+//         <main className="flex min-h-screen flex-col items-center justify-center p-24">
+//             <h1 className="text-xl mb-4">Super secure home page</h1>
+//             <p>
+//                 Only <strong>{tokens?.decodedToken.email}</strong> holds the magic key to this kingdom!
+//             </p>
+//         </main>
+//     );
+// }
 
 
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Header/>
-        <div className="flex flex-col gap-[32px]">Không phải gia sư tốt</div>
-      </main>
 
-    </div>
-  );
+
+
+
+
+import { getTokens } from "next-firebase-auth-edge";
+import { cookies } from "next/headers";
+import { notFound } from "next/navigation";
+import { clientConfig, serverConfig } from "./config";
+import HomePage from "@/app/Homepage";
+
+
+export default async function Home() {
+    const tokens = await getTokens(await cookies(), {
+        apiKey: clientConfig.apiKey,
+        cookieName: serverConfig.cookieName,
+        cookieSignatureKeys: serverConfig.cookieSignatureKeys,
+        serviceAccount: serverConfig.serviceAccount,
+    });
+
+    if (!tokens) {
+        notFound();
+    }
+
+    return <HomePage email={tokens?.decodedToken.email} />;
 }
---
+++ firebase.ts
@@ -0,0 +1,4 @@
+import { initializeApp } from 'firebase/app';
+import { clientConfig } from '@/app/config';
+
+export const app = initializeApp(clientConfig);
