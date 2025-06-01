e82bcefeaffa71fe31d94ff5d7774128b68e089e
FatAlligator <amazarashi2001@gmail.com>
2025-06-01 19:58:28 +0700

    basic registraion, verification, logging in

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -3,6 +3,9 @@ export default function HelloPage() {
         <div>
             <h1>Hello Page</h1>
             <p>This is the hello page</p>
+            <div>
+
+            </div>
         </div>
     );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-
 import { FormEvent, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { app } from "../../firebase";
+import { app } from "@/firebase";
 
 export default function Login() {
     const [email, setEmail] = useState("");
